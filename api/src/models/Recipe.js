5107ea39b2e6b57aff617900d7390f5006769825
Alex Hernán Laulhe <109404316+ahlaulhee@users.noreply.github.com>
2023-06-16 18:13:55 -0300

    Routes, models and basic structure

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -1,12 +1,41 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('recipe', {
+  sequelize.define("recipe", {
+    id: {
+      // "id"
+      // type: DataTypes.INTEGER,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     name: {
+      // "title"
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    image: {
+      // "image"
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    summary: {
+      // summary
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    health_score: {
+      // "healthScore"
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    recipe: {
+      // "analyzedInstructions"
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false, // maybe it can be null
     },
   });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,48 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+router.get("/recipes/:idRecipe", async (req, res) => {
+  // https://api.spoonacular.com/recipes/716429/information
+  try {
+    const { idRecipe } = req.params;
+  } catch (error) {}
+});
+
+router.get("recipes/", async (req, res) => {
+  //https://api.spoonacular.com/recipes/complexSearch?query=name
+  try {
+    const { name } = req.query;
+  } catch (error) {}
+});
+
+router.post("/recipes", async (req, res) => {
+  try {
+    const { id, name, image, summary, health_score, recipe } = req.body;
+  } catch (error) {}
+});
+
+router.get("/diets", async (req, res) => {
+  const diets = [
+    "Gluten Free",
+    "Ketogenic",
+    "Vegetarian",
+    "Lacto-Vegetarian",
+    "Ovo-Vegetarian",
+    "Vegan",
+    "Pescetarian",
+    "Paleo",
+    "Primal",
+    "Low FODMAP",
+    "Whole30",
+  ];
+  try {
+  } catch (error) {}
+});
 
 module.exports = router;
