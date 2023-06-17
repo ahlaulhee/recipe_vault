d82d285c384ef369f05e10b54ce8227c9964cc15
Alex Hernán Laulhe <109404316+ahlaulhee@users.noreply.github.com>
2023-06-16 21:56:41 -0300

    Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Landing, Home, Form, Detail, Error } from "./pages";
+// import Landing from "./pages/Landing";
+// import Home from "./pages/Home";
+// import Form from "./pages/Form";
+// import Detail from "./pages/Detail";
+// import Error from "./pages/Error";
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Food</h1>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/form" element={<Form />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </div>
   );
 }
